c9fec7328c43b31a5446a0a2caf27fa2fbda1d1b
unknown <luchoherrerafernandez@gmail.com>
2021-05-26 17:15:34 -0300

    crud de mongo y nodejs terminado

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+const router = Router();
+const { 
+    renderNotesForm,
+    createdNewNote, 
+    renderNotes, 
+    renderEditForm, 
+    updateNote, 
+    deleteNote 
+} = require('../controllers/notes.controller');
+
+// NEW NOTE
+
+router.get('/notes/add', renderNotesForm);
+router.post('/notes/new-note', createdNewNote);
+
+//GET ALL NOTE
+router.get('/notes',renderNotes);
+
+//EDIT NOTES
+router.get('/notes/edit/:id',renderEditForm);
+router.post('/notes/edit/:id',updateNote);
+
+//DELETE NOTES
+router.delete('/notes/delete/:id',deleteNote);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+
 
 //Initializations
 
@@ -9,24 +13,32 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
-
+app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+app.use(methodOverride('_method'));
 
 //Global Variables
 
-
-
 //Routes
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+app.use(require('./routes/index.routes'));
+app.use(require('./routes/notes.routes'));
 
 //Static files
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 module.exports = app;
